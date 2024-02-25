d1d202e3a43b01588c52cc77f3bb03bf153a1d4e
Arham <arhamnasir088@gmail.com>
2024-02-25 17:00:05 +0500

    fixing ser-porxy

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -4,24 +4,36 @@ const httpProxy = require("http-proxy");
 const app = express();
 const PORT = 7000;
 
-const BASE_PATH = "http://localhost:3000";
-
 const proxy = httpProxy.createProxy();
+// let BASE_PATH = "http://localhost:3000"; // Default BASE_PATH
+let BASE_PATH; // Default BASE_PATH
 
 app.use((req, res) => {
+  // Example: You might want to extract it from the request or perform a DB query
   const hostname = req.hostname;
   const subdomain = hostname.split(".")[0];
 
-  // Custom Domain - DB Query
+  const url = req.url?.trim();
+  console.log("🚀 ~ app.use ~ url === ", typeof url);
+  if (url === "api1") {
+    // Example: You can use subdomain to dynamically set BASE_PATH
+    BASE_PATH = "http://localhost:3001";
+  } else if (url === "api1") {
+    BASE_PATH = "http://localhost:3002";
+  }
+  console.log("🚀 ~ app.use ~ BASE_PATH:", BASE_PATH);
 
-  const resolvesTo = `${BASE_PATH}/api/v1/`;
-
-  proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+  // Proxy the request to the determined BASE_PATH
+  return proxy.web(req, res, { target: BASE_PATH, changeOrigin: true });
 });
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
-  //   const url = req.url;
-  //   if (url === "/") proxyReq.path += "index.html";
+  // You can still modify the request if needed
+  const url = req.url;
+  if (url === "/3000") proxyReq.path = "http://localhost:3000";
+  console.log("🚀 ~ proxy.on ~ url:", proxyReq.path);
+  if (url === "/5000") proxyReq.path = "/";
+  console.log("🚀 ~ proxy.on ~ url:", proxyReq.path);
 });
 
 app.listen(PORT, () =>
